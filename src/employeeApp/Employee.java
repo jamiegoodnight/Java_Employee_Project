27492c0766da3c91f836a773ef9d50070e67723a
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-04-29 15:09:09 -0400

    Initial commit

---
+++ src/employeeApp/Employee.java
@@ -0,0 +1,100 @@
+package employeeApp;
+
+public class Employee {
+    // ------- fields
+    // base types in Java are lowercase. Strings are classes dervied from char
+    // and capitalized 
+
+    private int id;
+    private String fname;
+    private String lname;
+    private double salary;
+
+    // doubles are real numbers / floats 
+
+    private boolean has401k;
+    private int companyId;
+    private int healthPlanId;
+
+    // constructor -- instatiates the state of object
+
+    public Employee(String fname, String lname, double salary, boolean has401k, int companyId, int healthPlanId)
+    {
+        this.fname = fname;
+        this.lname = lname;
+        this.salary = salary;
+        this.has401k = has401k;
+        this.companyId = companyId;
+        this.healthPlanId = healthPlanId;
+    }
+
+    // ------ methods - getters and setters
+    public void setFname(String fname)
+    {
+        this.fname = fname;
+    }
+
+    // void means nothing is returned
+
+    public String getFname()
+    {
+        return fname;
+    }
+
+    public void setLname(String lname)
+    {
+        this.lname = lname;
+    }
+
+    public String getLname()
+    {
+        return lname;
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setHas401k(boolean has401k)
+    {
+        this.has401k = has401k;
+    }
+
+    public boolean getHas401k()
+    {
+        return has401k;
+    }
+
+    public void setCompanyId(int companyId)
+    {
+        this.companyId = companyId;
+    }
+
+    public int getCompanyId()
+    {
+        return companyId;
+    }
+
+    public void setHealthPlanId(int healthPlanId)
+    {
+        this.healthPlanId = healthPlanId;
+    }
+
+    public int getHealthPlanId()
+    {
+        return healthPlanId;
+    }
+
+    // ------- other methods 
+
+    public String getName()
+    {
+        return fname + " " + lname;
+    }
+}
---
+++ src/employeeApp/Main.java
@@ -0,0 +1,26 @@
+package employeeApp;
+
+public class Main 
+{
+    // fieds - information
+    // methods - behaviors, equivalent to JS functions
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    } 
+    // Every Java program has a public static void main
+}
+
+
+// $ javac employeeApp/*.java -- generated/compiled bytecode. The Java runtime env interprets bytecode for 
+// each system
+
+// cat employeeApp/Main.class -- so we can see Main.class
+
+//  jar cvfe EmpApp.jar employeeApp.Main employeeApp/*.class Java archive file, combines class files 
+
+//  java -jar EmpApp.jar
+
+//  ^^virtual machine / what kind of file / the file we're running 
+
